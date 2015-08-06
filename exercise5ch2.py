58fc8a8798d2f2ee37489f9bdf33f1c809e22357
Dustin Armstrong <dustinarmstark@Dustins-Mac-mini.local>
2015-08-05 22:28:37 -0400

    2nd commit

---
+++ exercise5ch2.py
@@ -0,0 +1,7 @@
+# Program that converts Celcius to Fahrenheit using the equation Celsius * 1.8 + 32 = Fahrenheit
+
+# To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32
+
+temperature = raw_input('What is the temperature outside today in Celsius?\n')
+
+print int(temperature) * 1.8 + 32.0
