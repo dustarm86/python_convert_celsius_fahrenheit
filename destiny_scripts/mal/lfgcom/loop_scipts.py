3018517427ee857dfac76e99cdac9591d243fba5
Dustin Armstrong <dustinarmstark@Dustins-Mac-mini.local>
2015-11-20 01:11:12 -0500

    first commit

---
+++ destiny_scripts/mal/lfgcom/loop_scipts.py
@@ -0,0 +1,32 @@
+"""
+import toolfmcom
+import kingsfallhmfreshlfmcom
+import oryxhmlfgcom
+import ironbannerlfmcom
+"""
+
+#scripts = ["toolfmcom.py", "kingsfallhmfreshlfmcom.py", "oryxhmlfgcom.py", "ironbannerlfmcom.py"]
+
+# infinite loop
+x = 9
+while x == 9:
+    execfile("toolfmcom.py")
+    execfile("kingsfallhmfreshlfmcom.py")
+    execfile("oryxhmlfgcom.py")
+    execfile("ironbannerlfmcom.py")
+
+
+
+
+"""
+print("Opening first scipt (ToO)\n")
+execfile("toolfmcom.py")
+print("Script Complete. Opening 2nd script (King's Fall HM Fresh)\n")
+execfile("kingsfallhmfreshlfmcom.py")
+print("Script Complete. Opening 3rd Script (Oryx CP)\n")
+execfile("oryxhmlfgcom.py")
+print("Script Complete. Opening 4th script (Iron Banner LFM)...\n")
+execfile("ironbannerlfmcom.py")
+print("All scripts complete. Starting over...\n")
+execfile("toolfmcom.py")
+"""
---
+++ destiny_scripts/mal/lfgcom/toolfmcom.py
@@ -0,0 +1,42 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+browser.get('http://destinylfg.com')
+assert 'DestinyLFG.com - The Bungie Featured Destiny LFG Site' in browser.title
+time.sleep(6)
+
+list_my_guardian = browser.find_element_by_xpath(".//*[@id='listMe']").click()
+time.sleep(1.5)
+
+select_console = browser.find_element_by_xpath(".//*[@id='selectConsole']/option[2]").click()
+select_language = browser.find_element_by_xpath(".//*[@id='selectRegion']/option[2]").click()
+
+psn_name = browser.find_element_by_xpath(".//*[@id='inputName']")
+psn_name.send_keys('Conscious_Acts')
+select_class = browser.find_element_by_xpath(".//*[@id='selectClass']/option[2]").click()
+level = browser.find_element_by_xpath(".//*[@id='inputLvl']")
+level.send_keys('40')
+select_light = browser.find_element_by_xpath(".//*[@id='selectLight']/option[2]").click()
+mic_select = browser.find_element_by_xpath(".//*[@id='listGuardian']/div[6]/div[1]/div/label").click()
+i_am_lfm = browser.find_element_by_xpath(".//*[@id='selectLF']/option[3]").click()
+
+# choose activity from list
+select_activity = browser.find_element_by_xpath(".//*[@id='selectActivity']/optgroup[8]/option[2]").click()
+
+# write description for activity chosen
+description = browser.find_element_by_xpath(".//*[@id='textComment']")
+description.send_keys("LFM +2.0KD player looking to help others get to the Lighthouse! Message and Friend Request me on PSN to Join! PSN = Conscious_Acts")
+
+list_me = browser.find_element_by_xpath(".//*[@id='listMeForm']").click()
+
+time.sleep(120)
+browser.quit()
+
+print("Script completed at:\n")
+print datetime.datetime.now()
+print("Opening next script now...\n")
+
+import kingsfallhmfreshlfmcom
